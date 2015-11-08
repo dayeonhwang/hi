b697044d2f0e01e3db44ac906c7859292ea2c83c
Da Yeon Hwang <dadoyeon@gmail.com>
2015-11-08 17:54:27 -0600

    change port number to 5000

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http').Server(app);
 app.get('/', function(req,res){
 	res.send('<h1>Hi</h1>');
 });
-http.listen(3000,function(){
-	console.log('listening on *:3000');
+http.listen(5000,function(){
+	console.log('listening on *:5000');
 });
