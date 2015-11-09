83a0286f7ec89b4474610f1fbc88168cf9ce21b2
Da Yeon Hwang <dadoyeon@gmail.com>
2015-11-08 19:54:13 -0600

    integrating socket.io

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 var app = require('express')();
 var http = require('http').Server(app);
-app.get('/', function(req,res){
-	res.send('<h1>Hi</h1>');
-});
 http.listen(5000,function(){
 	console.log('listening on *:5000');
+});
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+npm install --save socket.io
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/',function(req, res){
+	res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+http.listen(5000,function(){
+	console.log('listening on *:5000');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
