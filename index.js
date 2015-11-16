263682cdbbde72ff61b4185600060bab9afcdde2
Da Yeon Hwang <dadoyeon@gmail.com>
2015-11-15 20:09:56 -0600

    broadcasting

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
-http.listen(5000,function(){
-	console.log('listening on *:5000');
-});
-app.get('/',function(req,res){
-	res.sendFile(__dirname + '/index.html');
-});
-npm install --save socket.io
-
-var app = require('express')();
-var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/',function(req, res){
-	res.sendfile('index.html');
-});
-
-io.on('connection', function(socket){
-	console.log('a user connected');
+	res.sendFile('/Users/dayeonhwang/Documents/Hi/index.html');
 });
 
 http.listen(5000,function(){
@@ -29,3 +15,10 @@ io.on('connection', function(socket){
     console.log('message: ' + msg);
   });
 });
+io.emit('some event', { for: 'everyone'});
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+	
